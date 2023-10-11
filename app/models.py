a1b3a90fa03849208c700399ca6e86265161b21e
Yonathan Maldonado <yo.maldonado@duocuc.cl>
2023-10-10 21:57:03 -0300

    Conexion Base Datos SQL

--- app/models.py
+++ app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+#Clase Rol
+class Rol(models.Model):
+    id_rol = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50, unique=True)
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+        
+        
+#Clase Region
+class Region(models.Model):
+    id_region = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nombre
+        
+#Clase Comuna        
+class Comuna(models.Model):
+    id_comuna = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
