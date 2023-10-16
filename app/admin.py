1b4f2949657614677e98ebb064243b056d504e26
tamvilches <tam.vilches@duocuc.cl>
2023-10-16 17:56:41 -0300

    Congifuracion admin Django

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Rol, Region, Comuna, Medicamento, Receta, Persona, Usuario, Paciente, Cesfam, Despacho, Historial
 
 # Register your models here.
+admin.site.register(Rol)
+admin.site.register(Region)
+admin.site.register(Comuna)
+admin.site.register(Medicamento)
+admin.site.register(Receta)
+admin.site.register(Persona)
+admin.site.register(Usuario)
+admin.site.register(Paciente)
+admin.site.register(Cesfam)
+admin.site.register(Despacho)
+admin.site.register(Historial)
